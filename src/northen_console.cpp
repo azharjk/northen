27998f4ffdc23f65aef9668ffc7da314532c541e
azharjk <azhr.project@gmail.com>
2021-07-29 07:43:36 +0700

    Northen Console: Trying new AST another main is commentted

--- src/northen_console.cpp
+++ src/northen_console.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <memory>
 
 #include <northen/northen.h>
 
 // TODO: Add more feature for the console application
 
-int main(int, char** argv) {
-  N::Application app { argv[1] };
+// int main(int, char** argv) {
+//   N::Application app { argv[1] };
 
-  std::cout << "Generated Token" << std::endl;
-  for (auto v : app.tokens()) {
-    std::cout << v.ToString() << std::endl;
-  }
+//   std::cout << "Generated Token" << std::endl;
+//   for (auto v : app.tokens()) {
+//     std::cout << v.ToString() << std::endl;
+//   }
+// }
+
+// AST
+int main() {
+  auto program = std::make_shared<N::Program>(
+    std::make_shared<N::BinaryOperation>(
+      N::Operator::Plus,
+      std::make_shared<N::Number>(10),
+      std::make_shared<N::BinaryOperation>(
+        N::Operator::Multiply,
+        std::make_shared<N::Number>(10),
+        std::make_shared<N::Number>(10)
+      )
+    )
+  );
+
+  std::cout << program->Execute() << std::endl;
 }
