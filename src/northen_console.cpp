14ac19e7635689e0ef02af9fbbc2f5a4a9daf43f
azharjk <azhr.project@gmail.com>
2021-07-29 07:17:00 +0700

    Northen Console: Trying new Token::ToString method

--- src/northen_console.cpp
+++ src/northen_console.cpp
@@ -9,22 +9,6 @@ int main(int, char** argv) {
 
   std::cout << "Generated Token" << std::endl;
   for (auto v : app.tokens()) {
-    switch (v.type()) {
-      case N::TokenType::Number:
-        std::cout << "Number: " << v.data() << std::endl;
-        break;
-      case N::TokenType::Plus:
-        std::cout << "Plus" << std::endl;
-        break;
-      case N::TokenType::Minus:
-        std::cout << "Minus" << std::endl;
-        break;
-      case N::TokenType::Multiply:
-        std::cout << "Multiply" << std::endl;
-        break;
-      case N::TokenType::Devide:
-        std::cout << "Devide" << std::endl;
-        break;
-    }
+    std::cout << v.ToString() << std::endl;
   }
 }
